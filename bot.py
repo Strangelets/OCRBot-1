96c188d683bd500d829b1a23ce3d67e3c1bb9684
Tyler <77331166+Strangelets@users.noreply.github.com>
2021-10-10 15:59:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,14 +25,14 @@ START_TXT = """
 Hi {}
 I am OCR Bot.
 
-> `I can extract any text from images using OCR technology. All languages supported.`
+> `I can extract any text from images. All languages supported.`
 
 Send an image to get started.
 """
 
 START_BTN = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('Source Code', url='https://github.com/samadii/OCR-Bot'),
+        InlineKeyboardButton('Contact', url='https://telegram.dog/strangelets'),
         ]]
     )
 
@@ -50,7 +50,7 @@ async def start(bot, update):
 
 @Bot.on_message(filters.private & filters.photo)
 async def ocr(bot, msg):
-    lang_code = await bot.ask(msg.chat.id,'`Now send the ISO language code.`\n\n[List of ISO 639-2 language codes](https://en.m.wikipedia.org/wiki/List_of_ISO_639-2_codes)', filters=filters.text, parse_mode='Markdown', disable_web_page_preview=True)
+    lang_code = await bot.ask(msg.chat.id,'`Now send the language code.`\n\n[hin eng]()', filters=filters.text, parse_mode='Markdown', disable_web_page_preview=True)
     data_url = f"https://github.com/tesseract-ocr/tessdata/raw/main/{lang_code.text}.traineddata"
     dirs = '/app/vendor/tessdata/'
     path = f'{dirs}{lang_code.text}.traineddata'
